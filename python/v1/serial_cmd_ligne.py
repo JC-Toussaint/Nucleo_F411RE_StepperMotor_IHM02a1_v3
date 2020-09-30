4e676103e9687a4cb458876beda68494d225aa0b
JC-Toussaint <jean-christophe.toussaint@grenoble-inp.fr>
2020-09-30 14:50:22 +0200

    ajout python test pgm

---
+++ python/v1/serial_cmd_ligne.py
@@ -0,0 +1,13 @@
+from math import *
+from plotter import *
+ 
+if __name__ == "__main__":  
+    plotter_start()
+
+    dx=50000
+    dy=0
+    draw(dx, dy)
+        
+    gohome()
+    plotter_stop()
+
